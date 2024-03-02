dc495f7231df1dc70ba1fcec5bb530802aff3a18
clemcheyrou <clemcheyrou@gmail.com>
2024-03-02 17:41:04 +0100

    [ADD] navbar navigation

--- option1/src/App.tsx
+++ option1/src/App.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes as Router } from 'react-router-dom'
-import './App.css'
 
 import Dashboard from './pages/Dashboard/Dashboard'
 import PageNotFound from './pages/PageNotFound/PageNotFound'
+import Help from './pages/Help/Help'
 
 function App() {
   
   return (
       <Router>
         <Route index element={<Dashboard />}/>
+        <Route path="/help" element={<Help />}/>
         <Route path="*" element={<PageNotFound />} />
       </Router>
   )
--- option1/src/pages/Help/Help.tsx
+++ option1/src/pages/Help/Help.tsx
@@ -1,13 +1,13 @@
 import MainLayout from "../../components/Nav/MainLayout";
 
-function Dashboard() {
+function Help() {
   return (
 	<MainLayout>
 		<div className="flex flex-row text-red-400">
-			<p className="text-red-500">Dashboard</p>
+			<p className="text-red-500">Help</p>
 		</div>
 	</MainLayout>
   )
 }
 
-export default Dashboard
+export default Help
